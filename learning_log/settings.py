3f5e0bb5d63c3dcd8a1fb40bfe47159e06b4ada3
Landri <Landri_Robinson@sbcglobal.net>
2026-04-15 22:54:00 -0500

    Update Learning Log project

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'Mainapp',
     'users',
-    'bootstrap5',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Run migrations
+python manage.py migrate
+
+#start gunicorn
+gunicorn learning_log.wsgi:application
